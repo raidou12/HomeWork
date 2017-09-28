c7f0d1eb2bbd49c34ff0d8bb31cbf7e76e587596
raidou12 <30708037+raidou12@users.noreply.github.com>
2017-09-27 22:38:25 -0300

    OpenBeta27092017.2
    com Getts e Setts

--- src/Binario/Operations.java
+++ src/Binario/Operations.java
@@ -3,101 +3,96 @@ package Binario;
 import java.util.Scanner;
 
 public class Operations extends TestIn {
-	public void addition() {
-		Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
+	private char rest = '0';
+
+	public void ask() {
 		System.out.println("Primeiro numero(brinario)");
-		num1 = in.nextLine();
+		setNum1(in.nextLine());
 		System.out.println("Segundo numero(brinario)");
-		num2 = in.nextLine();
-		answer = "";
-		char rest = '0';
+		setNum2(in.nextLine());
+		setAnswer("");
+	}
 
+	public void addition() {
+		ask();
 		testLength();
-
-		for (int i = num1.length() - 1; i >= 0; i--) {
-			if (num1.charAt(i) == '0' && num2.charAt(i) == '0') {
+		for (int i = getNum1().length() - 1; i >= 0; i--) {
+			if (getNum1().charAt(i) == '0' && getNum2().charAt(i) == '0') {
 				if (rest == '0') {
-					answer = "0" + answer;
+					setAnswer("0" + getAnswer());
 				} else {
 					rest = '0';
-					answer = "1" + answer;
+					setAnswer("1" + getAnswer());
 				}
-			} else if (num1.charAt(i) == '1' && num2.charAt(i) == '0'
-					|| num1.charAt(i) == '0' && num2.charAt(i) == '1') {
+			} else if (getNum1().charAt(i) == '1' && getNum2().charAt(i) == '0'
+					|| getNum1().charAt(i) == '0' && getNum2().charAt(i) == '1') {
 				if (rest == '0') {
-					answer = "1" + answer;
+					setAnswer("1" + getAnswer());
 				} else {
 					rest = '1';
-					answer = "0" + answer;
+					setAnswer("0" + getAnswer());
 				}
-			} else if (num1.charAt(i) == '1' && num2.charAt(i) == '1') {
+			} else if (getNum1().charAt(i) == '1' && getNum2().charAt(i) == '1') {
 				if (rest == '0') {
 					rest = '1';
-					answer = "0" + answer;
+					setAnswer("0" + getAnswer());
 				} else {
 					rest = '1';
-					answer = "1" + answer;
+					setAnswer("1" + getAnswer());
 				}
 			}
 		}
 		if (rest == '1') {
-			answer = rest + answer;
+			setAnswer(rest + getAnswer());
 		}
-		System.out.println(num1 + "\n+ " + num2 + "\n" + answer);
+		System.out.println(getNum1() + "\n+ " + getNum2() + "\n" + getAnswer());
 
 	}
 
 	public void subtract() {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Primeiro numero(brinario)");
-		num1 = in.nextLine();
-		System.out.println("Segundo numero(brinario)");
-		num2 = in.nextLine();
-		answer = "";
-		char rest = '0';
-
+		ask();
 		testLength();
-
-		if (num1.equals(num2)) {
-			answer += "0";
+		if (getNum1().equals(getNum2())) {
+			setAnswer("0");
 		} else {
 			testNegative();
-			for (int i = num1.length() - 1; i >= 0; i--) {
-				if (num1.charAt(i) == '0' && num2.charAt(i) == '0') {
+			for (int i = getNum1().length() - 1; i >= 0; i--) {
+				if (getNum1().charAt(i) == '0' && getNum2().charAt(i) == '0') {
 					if (rest == '0') {
-						answer = "0" + answer;
+						setAnswer("0" + getAnswer());
 					} else {
-						answer = "1" + answer;
+						setAnswer("1" + getAnswer());
 					}
-				} else if (num1.charAt(i) == '1' && num2.charAt(i) == '1') {
+				} else if (getNum1().charAt(i) == '1' && getNum2().charAt(i) == '1') {
 					if (rest == '0') {
-						answer = "0" + answer;
+						setAnswer("0" + getAnswer());
 					} else {
 						rest = '0';
-						answer = "1" + answer;
+						setAnswer("1" + getAnswer());
 					}
-				} else if (num1.charAt(i) == '1' && num2.charAt(i) == '0') {
+				} else if (getNum1().charAt(i) == '1' && getNum2().charAt(i) == '0') {
 					if (rest == '0') {
-						answer = "1" + answer;
+						setAnswer("1" + getAnswer());
 					} else {
 						rest = '0';
-						answer = "0" + answer;
+						setAnswer("0" + getAnswer());
 					}
 				} else {
 					if (rest == '0') {
 						rest = '1';
-						answer = "1" + answer;
+						setAnswer("1" + getAnswer());
 					} else {
-						answer = "0" + answer;
+						setAnswer("0" + getAnswer());
 					}
 				}
 			}
 		}
-		if (negative == true) {
-			answer = "-" + answer;
-			System.out.println(num2 + "\n- " + num1 + "\n" + answer);
+		if (getNegative() == true) {
+			setAnswer("-" + getAnswer());
+			System.out.println(getNum2() + "\n- " + getNum1() + "\n" + getAnswer());
 		} else {
-			System.out.println(num1 + "\n- " + num2 + "\n" + answer);
+			System.out.println(getNum1() + "\n- " + getNum2() + "\n" + getAnswer());
 		}
 	}
 }
--- src/Binario/TestIn.java
+++ src/Binario/TestIn.java
@@ -1,40 +1,71 @@
 package Binario;
 
 public class TestIn {
-	
-	String num1;
-	String num2;
-	Boolean negative = false;
-	String answer;
-	
+
+	private String num1 = "";
+	private String num2 = "";
+	private Boolean negative = false;
+	private String answer = "";
+
 	public void testLength() {
 		boolean equals = true;
 		while (equals == true) {
-			if (num1.length() == num2.length()) {
+			if (getNum1().length() == getNum2().length()) {
 				equals = false;
-			}else if (num1.length() < num2.length()) {
-				num1 = "0"+num1;
+			} else if (num1.length() < num2.length()) {
+				setNum1("0" + getNum1());
 				equals = true;
-			}else {
-				num2 = "0"+num2;
+			} else {
+				setNum2("0" + getNum2());
 				equals = true;
 			}
 		}
-		
+
 	}
-	
+
 	public void testNegative() {
 		int number1 = Integer.parseInt(num1, 2);
 		int number2 = Integer.parseInt(num2, 2);
-		if (number1 < number2){
+		if (number1 < number2) {
 			String replace = num1;
 			String replace2 = num2;
-			num2 = replace;
-			num1 = replace2;
-			negative = true;
-			System.out.println("Diminuiu");
+			setNum2(replace);
+			setNum1(replace2);
+			setNegative(true);
 		}
-		
+
+	}
+
+	public String getNum1() {
+		return num1;
+	}
+
+	public void setNum1(String num1) {
+		this.num1 = num1;
+	}
+
+	public String getNum2() {
+		return num2;
+	}
+
+	public void setNum2(String num2) {
+		this.num2 = num2;
+	}
+
+	public Boolean getNegative() {
+		return negative;
+	}
+
+	public void setNegative(Boolean negative) {
+		this.negative = negative;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
 	}
 
 }
